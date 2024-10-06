1248485ea1531961a3717bc947645c6f51b81908
JangIn-40 <jih105802@gmail.com>
2024-10-06 23:14:03 +0900

    Initial Setting
    기본 세팅

---
+++ Source/ProjectDMD/ProjectDMD.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectDMD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectDMD, "ProjectDMD" );
